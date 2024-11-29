aad323863a71028be14a2594d79d4f3dea7b432e
Kachain Fuangsung <kachain.f@outlook.com>
2024-11-29 10:10:29 +0700

    Fix sphinx docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,39 +6,34 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-import pathlib
-import sys
-
-sys.path.insert(0, pathlib.Path(__file__).parents[2].resolve().as_posix())
-
 project = "modular-trader"
-copyright = "2024, kfuangsung"
-author = "kfuangsung"
-release = "0.0.2"
+copyright = "2024, Kachain Fuangsung"
+author = "Kachain Fuangsung"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    "sphinx.ext.duration",
     "sphinx.ext.autodoc",
-    "sphinx.ext.todo",
-    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.githubpages",
+    "sphinx.ext.autosectionlabel",
     "sphinx.ext.autosummary",
+    "sphinx_copybutton",
 ]
-autosummary_generate = True  # Turn on sphinx.ext.autosummary
 
 templates_path = ["_templates"]
 exclude_patterns = []
-
+autoclass_content = "both"
+autosummary_generate = True
+autodoc_default_options = {"exclude-members": "__init__"}
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "furo"
+html_title = "Modular Trader"
+html_theme = "pydata_sphinx_theme"
 html_static_path = ["_static"]
-
-
 html_theme_options = {
     "icon_links": [
         {
